8114310d3b2d159aad15f22d700ed5c9c992f0f3
tpyc12 <tpyc12@yandex.ru>
2022-04-15 19:08:36 +0300

    Initial commit.

---
+++ app/src/main/java/com/myhome/android/weatherappkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.myhome.android.weatherappkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
